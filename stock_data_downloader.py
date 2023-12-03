5014f29fc4d75265ec83f1c62b66df25addfe6fd
Julian Carrasquillo <114718681+Curly-Haired-Saiyan@users.noreply.github.com>
2023-12-02 19:22:54 -0600

    Update stock_data_downloader.py
    Added Main() function to hold all GUI elements.
    
    converted certain GUI elements to global variable to allow other parts of the code to access them.
    
    Created stack to store last 10  downloaded stock symbols
    
    Set default date to whatever todays date is (incase no values are selected for start and end date)
    
    added function to allow a user to see what the last 10 selected symbols are

--- stock_data_downloader.py
+++ stock_data_downloader.py
@@ -4,6 +4,8 @@ import datetime as dt
 import tkinter as tk
 from tkinter import messagebox, filedialog
 
+last_symbols = list()
+
 def GetStockData(symbol, start_date, end_date, save_path):
     data = yf.download(symbol, start=start_date, end=end_date)
     file_path = f"{save_path}/{symbol}.csv"
@@ -18,12 +20,28 @@ def SaveStockData(symbols, start_date, end_date, save_path):
         stock_data_display.insert(tk.END, f"Data for {symbol}: \n")
         stock_data_display.insert(tk.END, data.to_string(index=False) + "\n\n")
         
+        global last_symbols
+        if len(last_symbols) == 10:
+            last_symbols.pop(0)
+        last_symbols.append(symbol)
+
 def RetrieveInput():
+    global last_symbols
+
     symbol_input = symbol_entry.get()
     symbols = [symbol.strip() for symbol in symbol_input.split(",")]
     
+    if not symbol_input:
+        if last_symbols:
+            symbols = [last_symbols[-1]]
+
     start_date = start_date_var.get()
     end_date = end_date_var.get()
+
+    if not start_date:
+        start_date = dt.datetime.now().strftime("%m/%d/%Y")
+    if not end_date:
+        end_date = dt.datetime.now().strftime("%m/$d/%Y")
     
     try:
         start_date = dt.datetime.strptime(start_date, "%m/%d/%Y")
@@ -40,7 +58,21 @@ def RetrieveInput():
     except ValueError:
         messagebox.showerror("Error", "Please enter dates n MM/DD/YYYY format.")
 
-if __name__ == "__main__":
+def DisplayLastSymbols():
+    global last_symbols
+
+    top = tk.Toplevel()
+    top.title("Last Entered Symbols")
+
+    if last_symbols:
+        symbols_text = "\n".join(last_symbols[-10:])
+        label = tk.Label(top, text=symbols_text)
+        label.pack()
+    else:
+        label = tk.Label(top, text="No symbols entered yet.")
+        label.pack()
+
+def Main():
     # GUI window
     root = tk.Tk()
     root.title("Stock Data Downloader")
@@ -48,18 +80,21 @@ if __name__ == "__main__":
     # Symbol input field
     symbol_label = tk.Label(root, text="Enter Stock symbol(s) seperated by a comma")
     symbol_label.pack()
+    global symbol_entry
     symbol_entry = tk.Entry(root)
     symbol_entry.pack()
 
     # Date range selection
     start_date_label = tk.Label(root, text="Start date (MM/DD/YYYY):")
     start_date_label.pack()
+    global start_date_var
     start_date_var = tk.StringVar()
     start_date_entry = tk.Entry(root, textvariable=start_date_var)
     start_date_entry.pack()
 
     end_date_label = tk.Label(root, text="End Date (MM/DD/YYYY):")
     end_date_label.pack()
+    global end_date_var
     end_date_var = tk.StringVar()
     end_date_entry = tk.Entry(root, textvariable=end_date_var)
     end_date_entry.pack()
@@ -71,7 +106,14 @@ if __name__ == "__main__":
     # display area for stock data
     stock_data_label = tk.Label(root, text="Stock Data Preview")
     stock_data_label.pack()
+    global stock_data_display
     stock_data_display = tk.Text(root, height=15, width = 60)
     stock_data_display.pack()
+    
+    view_symbols_button = tk.Button(root, text="View Last entered Symbols", command=DisplayLastSymbols)
+    view_symbols_button.pack()
 
     root.mainloop()
+
+if __name__ == "__main__":
+    Main()
